3efb8bf4801f931211c3a69b964a5ff29da5d58b
MikeyHall <wangyimiao666@gmail.com>
2021-10-17 17:53:13 +0800

    基础

--- 01 Dart基本语法/demo14/05 Dart 导入Pub包管理系统中的库.dart
+++ 01 Dart基本语法/demo14/05 Dart 导入Pub包管理系统中的库.dart
@@ -21,9 +21,9 @@ pub包管理系统:
 
 5、看文档引入库使用
 */
-import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
-import 'package:date_format/date_format.dart';
+// import 'dart:convert' as convert;
+// import 'package:http/http.dart' as http;
+// import 'package:date_format/date_format.dart';
 
 main() async {
   // var url = "http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1";
@@ -32,14 +32,11 @@ main() async {
   //   var response = await http.get(url);
   //   if (response.statusCode == 200) {
   //     var jsonResponse = convert.jsonDecode(response.body);
-     
+
   //     print(jsonResponse);
   //   } else {
   //     print("Request failed with status: ${response.statusCode}.");
   //   }
 
-
-  
-    print(formatDate(DateTime(1989, 2, 21), [yyyy, '*', mm, '*', dd]));
-
+  // print(formatDate(DateTime(1989, 2, 21), [yyyy, '*', mm, '*', dd]));
 }
